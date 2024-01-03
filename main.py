24d0ec93a088c537f52769dc67135d82cfe15186
Musab <musabs2802@gmail.com>
2024-01-03 12:29:36 +0400

    inital commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+
+
+class RSSReader:
+    def __init__(self, url: str) -> None:
+        self.page = requests.get(url)
+        self.__extract()
+
+    def __extract(self):
+        soup = BeautifulSoup(self.page.content, "xml")
+
+        if not soup.find("channel"):
+            return
+
+        channel = soup.channel
+
+        if not channel.find("title"):
+            return
+        
+        print("Page Title:", channel.title.text)
+        print("Description: ", channel.description.text)
+        print("Link: ", channel.link.text)
+        print("-" * 100)
+        print()
+
+        items = soup.find_all("item")
+        for id, item in enumerate(items):
+            print("{}. Title:".format(id + 1), item.title.text)
+            if item.description:
+                print("Description: ", item.description.text)
+            print("link: ", item.link.text)
+            print()
+
+
+if __name__ == "__main__":
+    RSSReader("http://rss.cnn.com/rss/cnn_topstories.rss")
+    
